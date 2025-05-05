9e0858cb69c04f6edfb971e94ca03a70676a2a50
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-05-05 14:50:35 +0800

    update experiments

--- run.sh
+++ run.sh
@@ -28,17 +28,21 @@ torchrun --standalone --nproc_per_node=4 train_pcgrad.py --positive_scale_factor
 # experiment (3.a.5) | SGP w. ps=1.2 ns=1.0 | (Bigger oscillation will further reduce MBE level?)
 torchrun --standalone --nproc_per_node=4 train_pcgrad.py --positive_scale_factor 1.2 --negative_scale_factor 1.0 --batch_size=32
 
+# experiment (3.a.6) | Stop MBE momentum | (Will stopping MBE momentum hurts learning?)
+torchrun --standalone --nproc_per_node=4 train_pcgrad.py --positive_scale_factor 0.01 --negative_scale_factor 0.01 --batch_size=32 
+
+# experiment (3.a.7) | Maximize MBE | additive composition | (Will maximizing MBE hurt learning?)
+torchrun --standalone --nproc_per_node=4 train_pcgrad.py --additive_grad --inverse_ib_target --batch_size=32
+
 # --------------------------------------------------------------------------------------------------------------------------
 # Algorithm (b). Bio inspired memorization & compression "gated phase transition" ||  Explicit Phase transition improves learning ? 
 # --------------------------------------------------------------------------------------------------------------------------
-
 
 # - experiment (3.b) | gated phase transition | SGP w. ps=1.1 ns=0.9
 torchrun --standalone --nproc_per_node=4 train_pcgrad.py --positive_scale_factor 1.1 --negative_scale_factor 0.9 --switch_phase --batch_size=32
 
 # - experiment (3.b.1) | gated phase transition | pure regularization | additive composition | Does phase change make it less shitty? 
 torchrun --standalone --nproc_per_node=4 train_pcgrad.py --additive_grad --switch_phase --batch_size=32
-
 
 # - experiment (3.b.5) | gated phase transition | minimize MBE | additive compostion | use prior weights | MBE weight affects performance? 
 torchrun --standalone --nproc_per_node=4 train_pcgrad.py --additive_grad --switch_phase --use_prior_weights --batch_size=32
@@ -52,27 +56,4 @@ torchrun --standalone --nproc_per_node=4 train_pcgrad.py --additive_grad --switc
 # - experiment (3.b.8) | gated phase transition | minimize MBE | additive composition | randomize patch size (32 -> 896) | Bigger patch size improves performance? 
 torchrun --standalone --nproc_per_node=4 train_pcgrad.py --additive_grad --switch_phase --patch_schedule --init_patch_size 32 --batch_size=32
 
-
-# --------------------------------------------------------------------------------------------------------------------------------------------
-# Algorithm (c). Potential Based Reward Shaping (RL-view) ||  Does heoretical approach for Constraint minimization actually reduce MBE? 
-# --------------------------------------------------------------------------------------------------------------------------------------------
-
-# Invariant optimal policy gives better MBE regularization ? 
-
-# - experiment (3.c) | potential based reward shaping (PBRS) | rotate between DiffMBE (layer 2~9) & Entropy | additive grad composition | batch size 16
-# torchrun --standalone --nproc_per_node=4 train_pcgrad.py --additive_grad --diff_mbe --batch_size=16
-
-# - experiment (3.c.1) | potential based reward shaping (PBRS) | rotate between DiffMBE (layer 2~9) & Entropy | additive grad composition | batch size 32
-torchrun --standalone --nproc_per_node=4 train_pcgrad.py --additive_grad --diff_mbe --batch_size=32
-
-# - experiment (3.c.2) | PBRS | rotate between DiffMBE (layer 2~9) & Entropy | gated phase transition | additive grad composition | batch 32
-torchrun --standalone --nproc_per_node=4 train_pcgrad.py --additive_grad --diff_mbe --switch_phase --batch_size=32
-
-# - experiment (3.c.3) | PBRS | rotate between DiffMBE (layer 2~9) & Entropy | SGP w. ps=1.1 ns=0.9 | batch 32
-torchrun --standalone --nproc_per_node=4 train_pcgrad.py --positive_scale_factor 1.1 --negative_scale_factor 0.9 --batch_size=32
-
-# - experiment (3.c.4) | PBRS | rotate between DiffMBE (layer 2~9) & Entropy | SGP w. ps=1.1 ns=0.9 | batch 32 | gated phase transition
-torchrun --standalone --nproc_per_node=4 train_pcgrad.py --positive_scale_factor 1.1 --negative_scale_factor 0.9 --switch_phase --batch_size=32
-
-# Add grad info for better inspection 
-# torchrun --standalone --nproc_per_node=4 train_pcgrad.py --positive_scale_factor 1.1 --negative_scale_factor 1.1 --batch_size=32 --log_grad_info
+# - experiment (3.b.9) | cyclic pattern ?
