fix on abstract logit masking error

sorl/gat_sim.py
@@ -149,7 +149,7 @@ def extract_and_sample(logits, idx, recursion_mask, vocab_sizes, temperature):
     recursion_logits = logits[predict_mask]
     
     abstract_start = vocab_sizes[0]
-    recursion_logits[:, abstract_start:] = float('-inf')
+    recursion_logits[:, :abstract_start] = float('-inf')
     
     if temperature == 0.0:
         new_tokens = torch.argmax(recursion_logits, dim=-1)

sorl/neo_utils.py
@@ -6,8 +6,8 @@ from sorl.gat_sim import BOS_TOKEN_ID, GAT, recursion
 def infer_rythmic_insert_mask(tokens, K):
     batch_size, seq_len = tokens.shape
     assert batch_size == 1, "only one sample supported"
-    positions = torch.arange(seq_len, device=tokens.device).unsqueeze(0)
-    
+    positions = torch.arange(1, seq_len + 1, device=tokens.device).unsqueeze(0)
+
     is_bos = (tokens == BOS_TOKEN_ID).long()
     bos_cumsum = is_bos.cumsum(dim=1)
     bos_offsets = torch.zeros(batch_size, seq_len + 1, device=tokens.device, dtype=torch.long)