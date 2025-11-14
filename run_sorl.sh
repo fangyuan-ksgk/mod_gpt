1a51d96e989999fff2fdea25b099cbb72fc8e3e9
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-11-14 16:09:06 +0800

    update script (w/o GAPT)

--- run_sorl.sh
+++ run_sorl.sh
@@ -123,3 +123,65 @@ torchrun \
   --exploration_fraction $EXPLORE_FRAC \
   --use_static_memory_span \
   --run_info "cycle=${CYCLE_STEPS}, explore=${EXPLORE_FRAC}, mode=$MODE, exploit with select-one-SoRL + alpha=0.1"
+
+
+# Include GAPT
+torchrun \
+  --nproc_per_node=$N_GPUS \
+  --master_addr=$MASTER_ADDR \
+  --master_port=$MASTER_PORT \
+  train_sorl_v2.py \
+  --batch_size $BATCH_SIZE \
+  --train_seq_len $TRAIN_SEQ_LEN \
+  --val_seq_len $VAL_SEQ_LEN \
+  --num_iterations $NUM_ITERATIONS \
+  --num_rollouts $NUM_ROLLOUTS \
+  --alpha_loss $ALPHA_LOSS \
+  --mode $MODE \
+  --steps_per_cycle $CYCLE_STEPS \
+  --exploration_fraction $EXPLORE_FRAC \
+  --use_off_policy_distillation \
+  --use_static_memory_span \
+  --use_gapt \
+  --traj_perplexity_patience 100 \
+  --run_info "cycle=${CYCLE_STEPS}, explore=${EXPLORE_FRAC}, mode=$MODE, exploit with off-policy distillation"
+
+
+torchrun \
+  --nproc_per_node=$N_GPUS \
+  --master_addr=$MASTER_ADDR \
+  --master_port=$MASTER_PORT \
+  train_sorl_v2.py \
+  --batch_size $BATCH_SIZE \
+  --train_seq_len $TRAIN_SEQ_LEN \
+  --val_seq_len $VAL_SEQ_LEN \
+  --num_iterations $NUM_ITERATIONS \
+  --num_rollouts $NUM_ROLLOUTS \
+  --alpha_loss $ALPHA_LOSS \
+  --mode $MODE \
+  --steps_per_cycle $CYCLE_STEPS \
+  --exploration_fraction $EXPLORE_FRAC \
+  --use_on_policy_distillation \
+  --use_static_memory_span \
+  --use_gapt \
+  --traj_perplexity_patience 100 \
+  --run_info "cycle=${CYCLE_STEPS}, explore=${EXPLORE_FRAC}, mode=$MODE, exploit with on-policy distillation"
+
+torchrun \
+  --nproc_per_node=$N_GPUS \
+  --master_addr=$MASTER_ADDR \
+  --master_port=$MASTER_PORT \
+  train_sorl_v2.py \
+  --batch_size $BATCH_SIZE \
+  --train_seq_len $TRAIN_SEQ_LEN \
+  --val_seq_len $VAL_SEQ_LEN \
+  --num_iterations $NUM_ITERATIONS \
+  --num_rollouts $NUM_ROLLOUTS \
+  --alpha_loss $ALPHA_LOSS \
+  --mode $MODE \
+  --steps_per_cycle $CYCLE_STEPS \
+  --exploration_fraction $EXPLORE_FRAC \
+  --use_static_memory_span \
+  --use_gapt \
+  --traj_perplexity_patience 100 \
+  --run_info "cycle=${CYCLE_STEPS}, explore=${EXPLORE_FRAC}, mode=$MODE, exploit with select-one-SoRL + alpha=0.1"
