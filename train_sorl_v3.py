b1016d43dc51d871fcbb8a53f41198ce1c6700d2
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-12-24 09:51:33 +0800

    update code

--- train_sorl_v3.py
+++ train_sorl_v3.py
@@ -508,33 +508,33 @@ for step in range(train_steps + 1):
                                                                     memory_span=memory_span, attn_blocksize=attn_blocksize, 
                                                                     temperature=temperature_val)
 
-                search_tokens, search_ppt, search_adv, _ = sorl_search(tokens, model, n=args.num_rollouts_val, K=K, max_iterations=args.max_iterations, 
-                                                                memory_span=memory_span, attn_blocksize=attn_blocksize, 
-                                                                temperature=temperature_val)
+                # search_tokens, search_ppt, search_adv, _ = sorl_search(tokens, model, n=args.num_rollouts_val, K=K, max_iterations=args.max_iterations, 
+                #                                                 memory_span=memory_span, attn_blocksize=attn_blocksize, 
+                #                                                 temperature=temperature_val)
 
                 greedy_util_rate = compute_vocab_utilization_rate(val_tokens, model)
-                search_util_rate = compute_vocab_utilization_rate(search_tokens, model)
+                # search_util_rate = compute_vocab_utilization_rate(search_tokens, model)
                 base_loss = model.forward(tokens, memory_span, attn_blocksize)[0].mean()
 
                 greedy_info_loss, greedy_abs_loss, greedy_zipf_loss = loss_fn(val_tokens, model, base_loss.detach(), memory_span, attn_blocksize)
-                search_info_loss, search_abs_loss, search_zipf_loss = loss_fn(search_tokens, model, base_loss.detach(), memory_span, attn_blocksize)
+                # search_info_loss, search_abs_loss, search_zipf_loss = loss_fn(search_tokens, model, base_loss.detach(), memory_span, attn_blocksize)
                 
                 greedy_traj_loss = greedy_info_loss + base_loss 
-                search_traj_loss = search_info_loss + base_loss 
+                # search_traj_loss = search_info_loss + base_loss 
                 greedy_rel_info_gain = -greedy_info_loss / base_loss 
-                search_rel_info_gain = -search_info_loss / base_loss 
+                # search_rel_info_gain = -search_info_loss / base_loss 
 
                 val_loss["base_traj_loss"] += base_loss.mean()
-                val_loss["cond_traj_loss (greedy)"] += greedy_ppt.mean()
-                val_loss["cond_traj_loss (search)"] += search_ppt.mean()
+                val_loss["cond_traj_loss (greedy)"] += greedy_traj_loss.mean()
+                # val_loss["cond_traj_loss (search)"] += search_traj_loss.mean()
                 val_loss["abs_loss (greedy)"] += greedy_abs_loss.mean()
-                val_loss["abs_loss (search)"] += search_abs_loss.mean()
+                # val_loss["abs_loss (search)"] += search_abs_loss.mean()
                 val_loss["greedy_adv"] += greedy_adv.mean()
-                val_loss["search_adv"] += search_adv.mean()
+                # val_loss["search_adv"] += search_adv.mean()
                 val_loss["info_gain (greedy)"] += greedy_rel_info_gain.mean()
-                val_loss["info_gain (search)"] += search_rel_info_gain.mean()
+                # val_loss["info_gain (search)"] += search_rel_info_gain.mean()
                 val_loss["util_rate (greedy)"] += torch.tensor(greedy_util_rate, device=greedy_traj_loss.device)
-                val_loss["util_rate (search)"] += torch.tensor(search_util_rate, device=search_traj_loss.device)
+                # val_loss["util_rate (search)"] += torch.tensor(search_util_rate, device=search_traj_loss.device)
                 val_loss["K"] += torch.tensor(float(K), device="cuda")  # Set (not +=), K is same for all val steps
             
         for name in val_loss: 
