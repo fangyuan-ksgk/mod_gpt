aa4db3f0e6af05d65a257f773dc46f6859647d45
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2026-01-07 11:58:49 +0800

    update iblm script: clamp on raw MBE loss

--- run_iblm.sh
+++ run_iblm.sh
@@ -90,29 +90,29 @@ for MBE_WEIGHT in 1.0 5.0 10.0; do
       --run_info "GAPT: ModelSize=$MODEL_SIZE | GAPT | w=20.0 | MBE comp mode: $MBE_COMP_MODE | regularize on all middle layers" 
 done
 
-
-# MODEL_SIZE="large"
-# for MBE_COMP_MODE in "decay" "spike" "naive" "max"; do
-#   torchrun \
-#       --nproc_per_node=$N_GPUS \
-#       --master_addr=$MASTER_ADDR \
-#       --master_port=$((MASTER_PORT++)) \
-#       train_iblm.py \
-#       --batch_size 32 \
-#       --train_seq_len $TRAIN_SEQ_LEN \
-#       --val_seq_len $VAL_SEQ_LEN \
-#       --num_iterations 1750 \
-#       --use_gapt \
-#       --entropy_patience 125 \
-#       --entropy_min_delta 0.01 \
-#       --mbe_patience 75 \
-#       --mbe_min_delta 0.01 \
-#       --mbe_weight 20.0 \
-#       --mbe_comp_mode $MBE_COMP_MODE \
-#       --mbe_schedule "all_middle" \
-#       --model_size $MODEL_SIZE \
-#       --run_info "GAPT: ModelSize=$MODEL_SIZE | GAPT | w=20.0 | MBE comp mode: $MBE_COMP_MODE | regularize on all middle layers" 
-# done
+# Just to validate that 'numerical instability' is gone with the raw MBE clamping
+MODEL_SIZE="large"
+for MBE_COMP_MODE in "naive"; do
+  torchrun \
+      --nproc_per_node=$N_GPUS \
+      --master_addr=$MASTER_ADDR \
+      --master_port=$((MASTER_PORT++)) \
+      train_iblm.py \
+      --batch_size 32 \
+      --train_seq_len $TRAIN_SEQ_LEN \
+      --val_seq_len $VAL_SEQ_LEN \
+      --num_iterations 1750 \
+      --use_gapt \
+      --entropy_patience 125 \
+      --entropy_min_delta 0.01 \
+      --mbe_patience 75 \
+      --mbe_min_delta 0.01 \
+      --mbe_weight 20.0 \
+      --mbe_comp_mode $MBE_COMP_MODE \
+      --mbe_schedule "all_middle" \
+      --model_size $MODEL_SIZE \
+      --run_info "GAPT: ModelSize=$MODEL_SIZE | GAPT | w=20.0 | MBE comp mode: $MBE_COMP_MODE | regularize on all middle layers" 
+done
 
 MBE_COMP_MODE="spike"
 for MBE_WEIGHT in 1.0 5.0 10.0; do
