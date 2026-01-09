0d0001fb076188572b6c4caad30fb3556dab371c
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2026-01-09 14:23:29 +0800

    fix code

--- src/gapt.py
+++ src/gapt.py
@@ -240,10 +240,7 @@ class GPT_pure(nn.Module):
     def __init__(self, config):
         super().__init__()
 
-        self.num_encoder_layers = config.n_layer // 2
-        self.num_decoder_layers = config.n_layer - self.num_encoder_layers 
-        self.skip_weights = nn.Parameter(torch.ones(self.num_decoder_layers))
-
+        self.num_layers = config.n_layer
         self.transformer = nn.ModuleDict(dict(
             wte = nn.Embedding(config.vocab_size, config.n_embd),
             h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
